e6b6113df4f547d9db3af263a7c63f4aa25111fe
SsoS2 <wndnjs9326@naver.com>
2023-11-01 19:06:40 +0900

    에러수정

--- Dapao/src/main/java/com/dapao/domain/Criteria.java
+++ Dapao/src/main/java/com/dapao/domain/Criteria.java
@@ -1,6 +1,5 @@
 package com.dapao.domain;
 
-
 /**
  * 페이징 처리를 계산하는 기준의 정보를 저장
  */
@@ -14,28 +13,6 @@ public class Criteria /*기준*/{
 		this.pageSize = 10;
 	}
 	
-
-	public Criteria(int page, int pageSize) {
-		this.page = page;
-		this.pageSize = pageSize;
-	}
-	
-	// 변수를 저장하기위한 목적아님
-	// mapper에서 호출되는 메서드 # {pageStart} 호출함
-	public int getPageStart() {
-		return (this.page-1) * pageSize;
-	}
-	
-	public int getPage() {
-		return page;
-	}
-
-	public int getPageSize() {
-		return pageSize;
-	}
-	
-	// set
-
 	public void setPage(int page) {
 		if(page <= 0) {
 			this.page = 1;
@@ -43,7 +20,7 @@ public class Criteria /*기준*/{
 		}
 		this.page = page;
 	}
-
+	
 	public void setPageSize(int pageSize) {
 		if(pageSize <= 0 || pageSize > 100) {
 			this.pageSize = 10;
@@ -51,7 +28,6 @@ public class Criteria /*기준*/{
 		}
 		this.pageSize = pageSize;
 	}
-
 
 	// 변수를 저장하기위한 목적 아님
 	// mapper에서 호출되는 메서드 # {pageStart }를 호출함
@@ -68,10 +44,10 @@ public class Criteria /*기준*/{
 		return pageSize;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Criteria [page=" + page + ", pageSize=" + pageSize + "]";
 	}
-
+	
 }
+
