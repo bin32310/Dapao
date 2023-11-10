e35b41985cfd93b0ab8fa81cff7d4d7000388b77
SsoS2 <wndnjs9326@naver.com>
2023-11-10 19:29:01 +0900

    admin DAO,Service 삭제

--- Dapao/src/main/java/com/dapao/persistence/AdminDAOImpl.java
+++ Dapao/src/main/java/com/dapao/persistence/AdminDAOImpl.java
@@ -9,23 +9,20 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.dapao.domain.AcVO;
-import com.dapao.domain.AdVO;
 import com.dapao.domain.Criteria;
 import com.dapao.domain.CsVO;
 import com.dapao.domain.EntVO;
 import com.dapao.domain.ItemVO;
 import com.dapao.domain.ProdVO;
 import com.dapao.domain.ExpVO;
-import com.dapao.domain.ItemVO;
-import com.dapao.domain.ProdVO;
 import com.dapao.domain.ReviewVO;
 import com.dapao.domain.UserVO;
 
-@Service
-public class AdminDAOImpl implements AdminDAO {
+@Repository(value = "AdminDAO")
+public class AdminDAOImpl{
 
 	// 디비연결객체 정보를 주입
 	private static final Logger logger = LoggerFactory.getLogger(AdminDAOImpl.class);
@@ -40,243 +37,234 @@ public class AdminDAOImpl implements AdminDAO {
 
 	private static final String NAMESPACE = "com.dapao.mapper.AdminMapper";
 
-	@Override
+	// 회원관리 - 개인유저정보 출력
 	public UserVO userInfo(String us_id) throws Exception {
 		logger.debug("DAO : userInfo(us_id) 호출");
 		return sqlSession.selectOne(NAMESPACE + ".userInfo", us_id);
 	}
 
-	@Override
+	// 회원관리 - 회원정지부여
 	public int userStop(UserVO vo) throws Exception {
 		logger.debug("DAO : userStop(String us_id, String us_stopdate) 호출");
 		return sqlSession.update(NAMESPACE + ".userStop", vo);
 	}
 
-	@Override
+	// 회원관리 - 회원정지(회원상태변경 0->1)
 	public void userStateUpdate(String us_id) throws Exception {
 		logger.debug("DAO : userStateUpdate(String us_id) 호출");
 		sqlSession.update(NAMESPACE + ".userStateUpdate", us_id);
 	}
 
-	@Override
+	// 회원관리 - 회원탈퇴(회원상태변경 0->2)
 	public int userDelete(String us_id) throws Exception {
 		logger.debug("DAO : userDelete(String us_id) 호출");
 		return sqlSession.update(NAMESPACE + ".userDelete", us_id);
 	}
 
-	@Override
+	// 회원관리 - 페이징 처리 후 회원 수 조회
 	public List<UserVO> getUserList(Criteria cri) throws Exception {
 		return sqlSession.selectList(NAMESPACE + ".userListCri", cri);
 	}
 
-	@Override
+	// 회원관리 - 전체회원수 조회
 	public int getUserCount() throws Exception {
 		return sqlSession.selectOne(NAMESPACE + ".userCount");
 	}
 
-	@Override
+	// 사업자 관리 - 사업자정보 리스트
 	public List<EntVO> ownerList(Criteria cri) throws Exception {
 		logger.debug(" DAO : ownerList() 호출");
 
 		return sqlSession.selectList(NAMESPACE + ".ownerList", cri);
 	}
 
-	@Override
+	// 사업자 관리 - 사업자 정보 출력
 	public EntVO ownerInfo(String own_id) throws Exception {
 		logger.debug("DAO : ownerInfo(String own_id) 호출");
 		return sqlSession.selectOne(NAMESPACE + ".ownerInfo", own_id);
 	}
 
-	@Override
+	// 사업자 관리 - 사업자 승인
 	public int ownerApprove(String own_id) throws Exception {
 		logger.debug("DAO : ownerApprove(String own_id) 호출");
 		return sqlSession.update(NAMESPACE + ".ownerApprove", own_id);
 	}
 
-	@Override
+	// 사업자 관리 - 사업자 탈퇴
 	public int ownerInfoDelete(String own_id) throws Exception {
 		logger.debug("DAO : ownerInfoDelete(String own_id) 호출");
 		return sqlSession.update(NAMESPACE + ".ownerInfoDelete", own_id);
 	}
-
-	@Override
-	public int noticeCount() throws Exception {
-		logger.debug("DAO : noticeCount() 호출");
-		return sqlSession.selectOne(NAMESPACE + ".noticeCount");
-	}
-
-	@Override
-	public List<CsVO> noticeList(Criteria cri) throws Exception {
-		logger.debug("DAO : noticeList() 호출");
-		return sqlSession.selectList(NAMESPACE + ".noticeList", cri);
-	}
-
-	@Override
-	public int csUpload(Integer cs_no) throws Exception {
-		logger.debug("DAO : csUpload(Integer cs_no) 호출");
-		return sqlSession.update(NAMESPACE + ".csUpload", cs_no);
-	}
-
-	@Override
-	public int csRemove(Integer cs_no) throws Exception {
-		logger.debug("DAO : csRemove(Integer cs_no) 호출");
-		return sqlSession.update(NAMESPACE + ".csRemove", cs_no);
-	}
-
-	@Override
-	public void noticeWrite(CsVO vo) throws Exception {
-		logger.debug("DAO : noticeWrite()  호출");
-		sqlSession.insert(NAMESPACE + ".noticeWrite", vo);
-	}
-
-	@Override
-	public int csDelete(Integer cs_no) throws Exception {
-		logger.debug("DAO : csDelete(Integer cs_no)  호출");
-		return sqlSession.delete(NAMESPACE + ".csDelete", cs_no);
-	}
-
-	@Override
+	
+	// 사업자 관리 - 사업자 총개수 조회
 	public int ownerCount(String own_id) throws Exception {
 		logger.debug(" DAO : ownerList() 호출");
 		return sqlSession.selectOne(NAMESPACE + ".ownerCount");
 	}
 
-	@Override
+	// 사업자 관리 - 사업자 정지
 	public int ownerStop(EntVO vo) throws Exception {
 		logger.debug("DAO : ownerStop(EntVO vo) 호출");
 		return sqlSession.update(NAMESPACE + ".ownerStop", vo);
 	}
 
-	@Override
-	public List<CsVO> FAQList(Criteria cri) throws Exception {
-		logger.debug("DAO : FAQList() 호출");
-		return sqlSession.selectList(NAMESPACE + ".FAQList", cri);
+	// FAQ&공지 관리 - notice 총 글개수 조회
+	public int noticeCount() throws Exception {
+		logger.debug("DAO : noticeCount() 호출");
+		return sqlSession.selectOne(NAMESPACE + ".noticeCount");
 	}
 
-	@Override
+	// FAQ&공지 관리 - notice 글 목록 리스트
+	public List<CsVO> noticeList(Criteria cri) throws Exception {
+		logger.debug("DAO : noticeList() 호출");
+		return sqlSession.selectList(NAMESPACE + ".noticeList", cri);
+	}
+
+	// FAQ&공지 관리 - notice 글쓰기
+	public void noticeWrite(CsVO vo) throws Exception {
+		logger.debug("DAO : noticeWrite()  호출");
+		sqlSession.insert(NAMESPACE + ".noticeWrite", vo);
+	}
+	
+	// FAQ&공지 관리 - cs 등록
+	public int csUpload(Integer cs_no) throws Exception {
+		logger.debug("DAO : csUpload(Integer cs_no) 호출");
+		return sqlSession.update(NAMESPACE + ".csUpload", cs_no);
+	}
+
+	// FAQ&공지 관리 - cs 등록해제
+	public int csRemove(Integer cs_no) throws Exception {
+		logger.debug("DAO : csRemove(Integer cs_no) 호출");
+		return sqlSession.update(NAMESPACE + ".csRemove", cs_no);
+	}
+
+	// FAQ&공지 관리 - cs 삭제
+	public int csDelete(Integer cs_no) throws Exception {
+		logger.debug("DAO : csDelete(Integer cs_no)  호출");
+		return sqlSession.delete(NAMESPACE + ".csDelete", cs_no);
+	}
+	
+	// FAQ&공지 관리 - cs 1개정보 출력
 	public CsVO csInfo(Integer cs_no) throws Exception {
 		logger.debug("DAO : csInfo(Integer cs_no)  호출");
 		return sqlSession.selectOne(NAMESPACE + ".csInfo", cs_no);
 	}
 
-	@Override
+	// FAQ&공지 관리 - cs 정보 수정
+	public int csInfoUpdate(CsVO vo) throws Exception {
+		logger.debug("DAO : csInfoUpdate(CsVO vo)  호출");
+		return sqlSession.update(NAMESPACE + ".csInfoUpdate", vo);
+	}
+	
+	// FAQ&공지 관리 - FAQ 리스트
+	public List<CsVO> FAQList(Criteria cri) throws Exception {
+		logger.debug("DAO : FAQList() 호출");
+		return sqlSession.selectList(NAMESPACE + ".FAQList", cri);
+	}
+
+	// FAQ&공지 관리 - FAQ 글쓰기
 	public void FAQWrite(CsVO vo) throws Exception {
 		logger.debug("DAO : FAQWrite()  호출");
 		sqlSession.insert(NAMESPACE + ".FAQWrite", vo);
 	}
 
-	@Override
-	public int csInfoUpdate(CsVO vo) throws Exception {
-		logger.debug("DAO : csInfoUpdate(CsVO vo)  호출");
-		return sqlSession.update(NAMESPACE + ".csInfoUpdate", vo);
-	}
-
-	@Override
+	// FAQ&공지 관리 - FAQ 총 글개수 조회
 	public int FAQCount() throws Exception {
 		logger.debug("DAO : FAQCount()  호출");
 		return sqlSession.selectOne(NAMESPACE + ".FAQCount");
 	}
 
-	@Override
+	// 리뷰관리 - 리뷰리스트
 	public List<ReviewVO> reviewList(Criteria cri) throws Exception {
 		logger.debug("DAO : reviewList(Criteria cri) 호출");
 		return sqlSession.selectList(NAMESPACE + ".reviewList", cri);
 	}
 
-	@Override
+	// 리뷰관리 - 리뷰 총 글개수 조회
 	public int reviewCount(Integer rv_no) throws Exception {
 		logger.debug("DAO : reviewCount(Integer rv_no) 호출");
 		return sqlSession.selectOne(NAMESPACE + ".reviewCount", rv_no);
 	}
 
-	@Override
+	// 리뷰관리 - 리뷰 1개정보 출력
 	public ReviewVO reviewInfo(Integer rv_no) throws Exception {
 		logger.debug("DAO : reviewInfo(Integer rv_no) 호출");
 		return sqlSession.selectOne(NAMESPACE + ".reviewInfo", rv_no);
 	}
 
-	@Override
+	// 리뷰관리 - 리뷰 삭제
 	public int reviewDelete(Integer rv_no) throws Exception {
 		logger.debug("DAO : reviewDelete(Integer rv_no) 호출");
 		return sqlSession.update(NAMESPACE + ".reviewDelete", rv_no);
 	}
 	
-	@Override
+	// 회원상품관리 - 총 상품 글개수 조회
 	public int itemCount() throws Exception {
 		logger.debug("itemCount() 호출");
 		return sqlSession.selectOne(NAMESPACE+".itemCount");
 	}
 	
-	@Override
+	// 회원상품관리  - 회원상품 목록 리스트
 	public List<ItemVO> itemList(Criteria cri) throws Exception {
 		logger.debug("itemList(Criteria cri) 호출");
 		return sqlSession.selectList(NAMESPACE+".itemList", cri);
 	}
 	
-	@Override
+	// 회원상품관리 - 상품삭제(글내리기)
 	public int itemDelete(Integer it_no) throws Exception {
 		logger.debug("itemDelete(Integer it_no)");
 		return sqlSession.update(NAMESPACE+".itemDelete", it_no);
 	}
 	
-	@Override
+	// 사업자상품관리 - 총 상품 글개수 조회
 	public int prodCount() throws Exception {
 		logger.debug("prodCount() 호출");
 		return sqlSession.selectOne(NAMESPACE+".prodCount");
 	}
 	
-	@Override
+	// 사업자상품관리  - 사업자상품 목록 리스트
 	public List<ProdVO> prodList(Criteria cri) throws Exception {
 		logger.debug("prodList(Criteria cri) 호출");
 		return sqlSession.selectList(NAMESPACE+".prodList", cri);
 	}
 	
-	@Override
+	// 회원상품관리 - 상품삭제(글내리기)
 	public int prodDelete(Integer prod_no) throws Exception {
 		logger.debug("prodDelete(Integer prod_no)");
 		return sqlSession.update(NAMESPACE+".prodDelete", prod_no);
 	}
 	
+	// 신고관리 - 신고 리스트
 	public List<AcVO> acList(Criteria cri) throws Exception {
 		logger.debug("DAO : acList(Criteria cri) 호출");
 		return sqlSession.selectList(NAMESPACE+".acList", cri);
 	}
 	
-	@Override
+	// 신고관리 - 신고 총 개수 조회
 	public int acCount(Integer ac_no) throws Exception {
 		logger.debug("DAO : acCount(Integer ac_no) 호출");
 		return sqlSession.selectOne(NAMESPACE+".acCount", ac_no);
 	}
 	
-	@Override
+	// 신고관리 - 신고 1개 글 정보
 	public AcVO acInfo(Integer ac_no) throws Exception {
 		logger.debug("DAO : acInfo(Integer ac_no) 호출");
 		return sqlSession.selectOne(NAMESPACE+".acInfo", ac_no);
 	}
 	
-	// 신고관리 - 접수 처리
-	@Override
-	public int acHandling(Integer ac_no) throws Exception {
-		logger.debug("DAO : acHandling(Integer ac_no) 호출");
-		return sqlSession.update(NAMESPACE+".acHandling", ac_no);
-	}
-
 	// 신고관리 - 신고 처리상태 업뎃(user 조회)
-	@Override
 	public String acResultSelectUserId(AcVO vo) throws Exception {
 		logger.debug("DAO : acResultSelectUserId(AcVO vo) 호출");
 		return sqlSession.selectOne(NAMESPACE+".acResultSelectUserId", vo);
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(owner 조회)
-	@Override
 	public String acResultSelectOwnerId(AcVO vo) throws Exception {
 		logger.debug("DAO : acResultSelectOwnerId(AcVO vo) 호출");
 		return sqlSession.selectOne(NAMESPACE+".acResultSelectOwnerId", vo);
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(user)
-	@Override
 	public int acResultUserUpdate(AcVO acVo,String stop) throws Exception {
 		logger.debug("DAO : acResultOwnerUpdate(AcVO acVo,String stop) 호출");
 		Map<String, Object> vo = new HashMap<String, Object>();
@@ -286,7 +274,6 @@ public class AdminDAOImpl implements AdminDAO {
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(owner)
-	@Override
 	public int acResultOwnerUpdate(AcVO acVo,String stop) throws Exception {
 		logger.debug("DAO : acResultOwnerUpdate(AcVO acVo,String stop) 호출");
 		Map<String, Object> vo = new HashMap<String, Object>();
@@ -297,49 +284,42 @@ public class AdminDAOImpl implements AdminDAO {
 	}
 	
 	//신고관리 - 신고 user 글 쓰기
-	@Override
 	public int acUserWrite(AcVO vo) throws Exception {
 		logger.debug("DAO :  acUserWrite(AcVO vo) 호출");
 		return sqlSession.insert(NAMESPACE+".acUserWrite",vo);
 	}
 	
 	//신고관리 - 신고 owner 글 쓰기
-	@Override
 	public int acOwnerWrite(AcVO vo) throws Exception {
 		logger.debug("DAO :  acOwnerWrite(AcVO vo) 호출");
 		return sqlSession.insert(NAMESPACE+".acOwnerWrite",vo);
 	}
 	
 	// 체험단관리 - 체험단 리스트
-	@Override
 	public List<ExpVO> expList(Criteria cri) throws Exception {
 		logger.debug("DAO :  expList(Criteria cri) 호출");
 		return sqlSession.selectList(NAMESPACE+".expList", cri);
 	}
 	
 	// 체험단관리 - 체험단 글개수 조회
-	@Override
 	public int expCount(Integer exp_no) throws Exception {
 		logger.debug("DAO :  expCount(Integer exp_no) 호출");
 		return sqlSession.selectOne(NAMESPACE+".expCount", exp_no);
 	}
 	
 	// 체험단관리 - 체험단 글 1개 정보
-	@Override
 	public ExpVO expInfo(Integer exp_no) throws Exception {
 		logger.debug("DAO :  expInfo(Integer exp_no) 호출");
 		return sqlSession.selectOne(NAMESPACE+".expInfo", exp_no);
 	}
 	
 	// 체험단관리 - 상태 업데이트
-	@Override
 	public void expStateUpdate(Integer exp_no) throws Exception {
 		logger.debug("DAO :  expStateUpdate(Integer exp_no) 호출");
 		sqlSession.update(NAMESPACE+".expStateUpdate", exp_no);
 	}
 	
 	// 체험단관리 - 광고테이블 insert
-	@Override
 	public int expAdInsert(String own_id, String ad_date) throws Exception {
 		logger.debug("DAO :  expAdInsert(String own_id, String ad_date)호출"+own_id+ad_date);
 		Map<String, Object> vo = new HashMap<String, Object>();
@@ -350,7 +330,6 @@ public class AdminDAOImpl implements AdminDAO {
 	}
 	
 	// 체험단관리 - 반려 상태 업뎃
-	@Override
 	public int expReturn(ExpVO vo) throws Exception {
 		logger.debug("DAO :  expReturn(ExpVO vo) 호출");
 		return sqlSession.update(NAMESPACE+".expReturn", vo);
--- Dapao/src/main/java/com/dapao/service/AdminServiceImpl.java
+++ Dapao/src/main/java/com/dapao/service/AdminServiceImpl.java
@@ -1,8 +1,6 @@
 package com.dapao.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -11,328 +9,305 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.dapao.domain.AcVO;
-import com.dapao.domain.AdVO;
 import com.dapao.domain.Criteria;
 import com.dapao.domain.CsVO;
 import com.dapao.domain.EntVO;
 import com.dapao.domain.ItemVO;
 import com.dapao.domain.ProdVO;
 import com.dapao.domain.ExpVO;
-import com.dapao.domain.ItemVO;
-import com.dapao.domain.ProdVO;
 import com.dapao.domain.ReviewVO;
 import com.dapao.domain.UserVO;
-import com.dapao.persistence.AdminDAO;
+import com.dapao.persistence.AdminDAOImpl;
 
-@Service
-public class AdminServiceImpl implements AdminService{
+@Service(value = "AdminService")
+public class AdminServiceImpl{
 	
 	private static final Logger logger = LoggerFactory.getLogger(AdminServiceImpl.class);
 	
 	@Inject
-	private AdminDAO adao;
+	private AdminDAOImpl adao;
 	
-	@Override
+	// 회원관리 - 개인유저정보 출력
 	public UserVO userInfo(String us_id) throws Exception {
 		logger.debug("service : userInfo(String us_id) 호출 ");
 		return adao.userInfo(us_id);
 	}
 	
-	@Override
+	// 회원관리 - 회원정지부여
 	public int userStop(UserVO vo) throws Exception {
 		logger.debug("service : userStop(UserVO vo) 호출 ");
 		return adao.userStop(vo);
 	}
 	
-	@Override
+	// 회원관리 - 회원정지(회원상태변경 0->1)
 	public void userStateUpdate(String us_id) throws Exception {
 		logger.debug("service : userStateUpdate(String us_id) 호출");
 		adao.userStateUpdate(us_id);
 	}
 	
-	@Override
+	// 회원관리 - 회원탈퇴(회원상태변경 0->2)
 	public int userDelete(String us_id) throws Exception {
 		logger.debug("service : userDelete(String us_id) 호출");
 		return adao.userDelete(us_id);
 	}
 	
-	@Override
+	// 회원관리 - 페이징처리 후 리스트조회
 	public List<UserVO> getUserList(Criteria cri) throws Exception {
 		return adao.getUserList(cri);
 	}
 	
-	@Override
+	// 회원관리 - 전체회원수조회
 	public int getUserCount() throws Exception {
 		return adao.getUserCount();
 	}
 	
-	@Override
+	// 사업자 관리 - 사업자정보 리스트
 	public List<EntVO> ownerList(Criteria cri) throws Exception{
 		logger.debug("service : ownerList() 호출");
 		return adao.ownerList(cri);
 	}
-
 	
-	@Override
+	// 사업자 관리 - 사업자 총개수 조회
+	public int ownerCount(String own_id) throws Exception {
+		logger.debug("service : ownerCount() 호출 ");
+		return adao.ownerCount(own_id);
+	}
+	
+	// 사업자 관리 - 사업자 정보 출력
 	public EntVO ownerInfo(String own_id) throws Exception {
 		logger.debug("service : ownerInfo(String own_id) 호출 ");
 		return adao.ownerInfo(own_id);
 	}
   
-  	@Override
-	public int ownerCount(String own_id) throws Exception {
-		logger.debug("service : ownerCount() 호출 ");
-		return adao.ownerCount(own_id);
-	}
-  	
-  	@Override
+	// 사업자 관리 - 사업자 정지
   	public int ownerStop(EntVO vo) throws Exception {
   		logger.debug("service : ownerStop(EntVO vo) 호출 ");
   		return adao.ownerStop(vo);
   	}
 	
-	@Override
+  	// 사업자 관리 - 사업자 승인
 	public int ownerApprove(String own_id) throws Exception {
 		logger.debug("service : ownerApprove(String own_id) 호출");
 		return adao.ownerApprove(own_id);
 	}
 	
-	@Override
+	// 사업자 관리 - 사업자 탈퇴
 	public int ownerInfoDelete(String own_id) throws Exception {
 		logger.debug("service : ownerInfoDelete(String own_id) 호출");
 		return adao.ownerInfoDelete(own_id);
 	}
 	
-	@Override
-	public List<CsVO> FAQList(Criteria cri) throws Exception {
-		logger.debug("service : FAQList() 호출");
-		return adao.FAQList(cri);
-	}
-	
-	@Override
+	// FAQ&공지 관리 - cs 1개정보 출력
 	public CsVO csInfo(Integer cs_no) throws Exception {
 		logger.debug("service : csInfo(Integer cs_no) 호출");
 		return adao.csInfo(cs_no);
 	}
 	
-	@Override
-	public void FAQWrite(CsVO vo) throws Exception {
-		logger.debug("service : FAQWrite() 호출");
-		adao.FAQWrite(vo);
-	}
-	
-	@Override
+	// FAQ&공지 관리 - cs 정보 수정
 	public int csInfoUpdate(CsVO vo) throws Exception {
 		logger.debug("service : csInfoUpdate(CsVO vo) 호출");
 		return adao.csInfoUpdate(vo);
 	}
 	
-	@Override
-	public int FAQCount() throws Exception {
-		logger.debug("service : FAQCount() 호출");
-		return adao.FAQCount();
-	}
-	
-	@Override
-	public int noticeCount() throws Exception {
-		logger.debug("service : noticeCount() 호출");
-		return adao.noticeCount();
-	}
-	
-	@Override
-	public List<CsVO> noticeList(Criteria cri) throws Exception {
-		logger.debug("service : noticeList() 호출");
-		return adao.noticeList(cri);
-	}
-	
-	@Override
+	// FAQ&공지 관리 - cs 등록
 	public int csUpload(Integer cs_no) throws Exception {
 		logger.debug("service : csUpload(Integer cs_no) 호출");
 		return adao.csUpload(cs_no);
 	}
 	
-	@Override
+	// FAQ&공지 관리 - cs 등록해제
 	public int csRemove(Integer cs_no) throws Exception {
 		logger.debug("service : csRemove(Integer cs_no) 호출");
 		return adao.csRemove(cs_no);
 	}
 	
-	@Override
-	public List<ReviewVO> reviewList(Criteria cri) throws Exception {
-		logger.debug("service : reviewList(Criteria cri) 호출");
-		return adao.reviewList(cri);
-	}
-	
-	@Override
-	public int reviewCount(Integer rv_no) throws Exception {
-		logger.debug("service : reviewCount(Integer rv_no) 호출");
-		return adao.reviewCount(rv_no);
-	}
-	
-	@Override
-	public ReviewVO reviewInfo(Integer rv_no) throws Exception {
-		logger.debug("service : reviewInfo(Integer rv_no)호출");
-		return adao.reviewInfo(rv_no);
-	}
-	
-	@Override
-	public int reviewDelete(Integer rv_no) throws Exception {
-		logger.debug("service : reviewDelete(Integer rv_no)호출");
-		return adao.reviewDelete(rv_no);
-	}
-  
-  @Override
-	public void noticeWrite(CsVO vo) throws Exception {
-		logger.debug("service : noticeWrite(CsVO vo) 호출");
-		adao.noticeWrite(vo);
-	}
-	
-	@Override
+	// FAQ&공지 관리 - cs 삭제
 	public int csDelete(Integer cs_no) throws Exception {
 		logger.debug("service : csDelete(Integer cs_no) 호출");
 		return adao.csDelete(cs_no);
 	}
 	
-	@Override
+	// FAQ&공지 관리 - FAQ 리스트
+	public List<CsVO> FAQList(Criteria cri) throws Exception {
+		logger.debug("service : FAQList() 호출");
+		return adao.FAQList(cri);
+	}
+	
+	// FAQ&공지 관리 - FAQ 글쓰기
+	public void FAQWrite(CsVO vo) throws Exception {
+		logger.debug("service : FAQWrite() 호출");
+		adao.FAQWrite(vo);
+	}
+	
+	// FAQ&공지 관리 - FAQ 총 글개수 조회
+	public int FAQCount() throws Exception {
+		logger.debug("service : FAQCount() 호출");
+		return adao.FAQCount();
+	}
+	
+	//  FAQ&공지 관리 - notice 총 글개수 조회
+	public int noticeCount() throws Exception {
+		logger.debug("service : noticeCount() 호출");
+		return adao.noticeCount();
+	}
+	
+	//  FAQ&공지 관리 - notice 글 목록 리스트
+	public List<CsVO> noticeList(Criteria cri) throws Exception {
+		logger.debug("service : noticeList() 호출");
+		return adao.noticeList(cri);
+	}
+	
+	//  FAQ&공지 관리 - notice 글쓰기	
+	public void noticeWrite(CsVO vo) throws Exception {
+		logger.debug("service : noticeWrite(CsVO vo) 호출");
+		adao.noticeWrite(vo);
+	}
+	
+	// 리뷰관리 - 리뷰리스트
+	public List<ReviewVO> reviewList(Criteria cri) throws Exception {
+		logger.debug("service : reviewList(Criteria cri) 호출");
+		return adao.reviewList(cri);
+	}
+	
+	// 리뷰관리 - 리뷰 총 글개수 조회
+	public int reviewCount(Integer rv_no) throws Exception {
+		logger.debug("service : reviewCount(Integer rv_no) 호출");
+		return adao.reviewCount(rv_no);
+	}
+	
+	// 리뷰관리 - 리뷰 1개정보 출력
+	public ReviewVO reviewInfo(Integer rv_no) throws Exception {
+		logger.debug("service : reviewInfo(Integer rv_no)호출");
+		return adao.reviewInfo(rv_no);
+	}
+	
+	// 리뷰관리 - 리뷰 삭제
+	public int reviewDelete(Integer rv_no) throws Exception {
+		logger.debug("service : reviewDelete(Integer rv_no)호출");
+		return adao.reviewDelete(rv_no);
+	}
+  
+	// 회원상품관리 - 총 상품 글개수 조회
 	public int itemCount() throws Exception {
 		logger.debug("service : itemCount() 호출");
 		return adao.itemCount();
 	}
 	
-	@Override
+	// 회원상품관리  - 회원상품 목록 리스트
 	public List<ItemVO> itemList(Criteria cri) throws Exception {
 		logger.debug("service : itemList(Criteria cri) 호출");
 		return adao.itemList(cri);
 	}
 	
-	@Override
+	// 회원상품관리 - 상품삭제(글내리기)
 	public int itemDelete(Integer it_no) throws Exception {
 		logger.debug("itemDelete(Integer it_no) 호출");
 		return adao.itemDelete(it_no);
 	}
 	
-	@Override
+	// 사업자상품관리 - 총 상품 글개수 조회
 	public int prodCount() throws Exception {
 		logger.debug("service : prodCount() 호출");
 		return adao.prodCount();
 	}
 	
-	@Override
+	// 사업자상품관리 - 사업자상품 목록 리스트
 	public List<ProdVO> prodList(Criteria cri) throws Exception {
 		logger.debug("service : prodList(Criteria cri) 호출");
 		return adao.prodList(cri);
 	}
 	
-	@Override
+	// 사업자상품관리 - 상품삭제(글내리기)
 	public int prodDelete(Integer prod_no) throws Exception {
 		logger.debug("service : prodDelete(Integer prod_no) 호출");
 		return adao.prodDelete(prod_no);
 	}
 
-  @Override
+	// 신고관리 - 신고 리스트
 	public List<AcVO> acList(Criteria cri) throws Exception {
 		logger.debug("service : acList(Criteria cri) 호출");
 		return adao.acList(cri);
 	}
 	
-	@Override
+	// 신고관리 - 신고 총 개수 조회
 	public int acCount(Integer ac_no) throws Exception {
 		logger.debug("service : acCount(Integer ac_no) 호출");
 		return adao.acCount(ac_no);
 	}
 	
-	@Override
+	// 신고관리 - 신고 1개 글 정보
 	public AcVO acInfo(Integer ac_no) throws Exception {
 		logger.debug("service : acInfo(Integer ac_no) 호출");
 		return adao.acInfo(ac_no);
 	}
 	
-	// 신고관리 - 접수 처리
-	@Override
-	public int acHandling(Integer ac_no) throws Exception {
-		logger.debug("service : acHandling(Integer ac_no) 호출");
-		return adao.acHandling(ac_no);
-	}
-	
 	// 신고관리 - 신고 처리상태 업뎃(user 조회)
-	@Override
 	public String acResultSelectUserId(AcVO vo) throws Exception {
 		logger.debug("service : acResultSelectUserId(AcVO vo) 호출");
 		return adao.acResultSelectUserId(vo);
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(owner 조회)
-	@Override
 	public String acResultSelectOwnerId(AcVO vo) throws Exception {
 		logger.debug("service : acResultSelectOwnerId(AcVO vo) 호출");
 		return adao.acResultSelectOwnerId(vo);
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(user)
-	@Override
 	public int acResultUserUpdate(AcVO acVo,String stop) throws Exception {
 		logger.debug("service : acResultUserUpdate(AcVO acVo,String stop) 호출");
 		return adao.acResultUserUpdate(acVo, stop);
 	}
 	
 	// 신고관리 - 신고 처리상태 업뎃(owner)
-	@Override
 	public int acResultOwnerUpdate(AcVO acVo,String stop) throws Exception {
 		logger.debug("service : acResultUserUpdate(AcVO acVo,String stop) 호출");
 		return adao.acResultOwnerUpdate(acVo, stop);
 	}
 	
 	//신고관리 - 신고 user 글 쓰기
-	@Override
 	public int acUserWrite(AcVO vo) throws Exception {
 		logger.debug("service : acUserWrite(AcVO vo) 호출");
 		return adao.acUserWrite(vo);
 	}
 	
 	//신고관리 - 신고 user 글 쓰기
-	@Override
 	public int acOwnerWrite(AcVO vo) throws Exception {
 		logger.debug("service : acOwnerWrite(AcVO vo) 호출");
 		return adao.acOwnerWrite(vo);
 	}
 	
 	// 체험단관리 - 체험단 리스트
-	@Override
 	public List<ExpVO> expList(Criteria cri) throws Exception {
 		logger.debug("service : expList(Criteria cri) 호출");
 		return adao.expList(cri);
 	}
 	
 	// 체험단관리 - 체험단 글개수 조회
-	@Override
 	public int expCount(Integer exp_no) throws Exception {
 		logger.debug("service : expCount(Integer exp_no) 호출");
 		return adao.expCount(exp_no);
 	}
 	
 	// 체험단관리 - 체험단 글 1개 정보
-	@Override
 	public ExpVO expInfo(Integer exp_no) throws Exception {
 		logger.debug("service : expInfo(Integer exp_no)호출");
 		return adao.expInfo(exp_no);
 	}
 	
 	// 체험단관리 - 상태 업데이트
-	@Override
 	public void expStateUpdate(Integer exp_no) throws Exception {
 		logger.debug("service : expStateUpdate(Integer exp_no) 호출");
 		adao.expStateUpdate(exp_no);
 	}
 	
 	// 체험단관리 - 광고테이블 insert
-	@Override
 	public int expAdInsert(String own_id, String ad_date) throws Exception {
 		logger.debug("service : expAdInsert(String own_id, String ad_date) 호출");
 		return adao.expAdInsert(own_id, ad_date);
 	}
 	
 	// 체험단관리 - 반려 상태 업뎃
-	@Override
 	public int expReturn(ExpVO vo) throws Exception {
 		logger.debug("service : expReturn(Integer exp_no) 호출");
 		return adao.expReturn(vo);
