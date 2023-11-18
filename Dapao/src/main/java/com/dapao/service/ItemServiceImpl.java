4fa02886ff80a346b4eb9622865c4304e1d94daf
bin32310 <dbs32310@naver.com>
2023-11-18 19:04:09 +0900

    user_exp_apply

--- Dapao/src/main/java/com/dapao/service/ItemServiceImpl.java
+++ Dapao/src/main/java/com/dapao/service/ItemServiceImpl.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.dapao.domain.AlarmVO;
+import com.dapao.domain.EntVO;
+import com.dapao.domain.ExpVO;
 import com.dapao.domain.ExpusVO;
 import com.dapao.domain.FileVO;
 import com.dapao.domain.ItemVO;
@@ -329,6 +331,13 @@ public class ItemServiceImpl {
 		
 	}
 	
+	// 체험단 정보 조회
+	public ExpVO getExpInfo(String own_id) {
+		logger.debug("Service : getExpInfo(String own_id) 호출");
+		return idao.getExpInfo(own_id);
+		
+	}
+
 	// 체험단리스트에 목록 추가
 	public int expApply(ExpusVO expusVO) {
 		logger.debug("Service : expApply(ExpusVO expusVO) 호출");
